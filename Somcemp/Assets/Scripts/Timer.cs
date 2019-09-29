f8fce22f971904f0ba1f25c896626b5614860578
Daniel Fróes <danielfroes15@gmail.com>
2019-09-29 15:11:11 -0300

    Game Over Prototipo

--- Somcemp/Assets/Scripts/Timer.cs
+++ Somcemp/Assets/Scripts/Timer.cs
@@ -7,11 +7,12 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     
-    private float currentSeconds = 0;
-    // [SerializeField] private float startingSecond;
-    private float currentMinutes = 0;
+    public static float currentSeconds = 0;
+    // [SerializeField] public static float startingSecond;
+    public static float currentMinutes = 0;
     [SerializeField] private TextMeshProUGUI secondsTextBox;
     [SerializeField] private TextMeshProUGUI minutesTextBox;
+    public static bool stopTimer = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +23,16 @@ public class Timer : MonoBehaviour
     void Update()
     {
         
-        if(currentSeconds >= 60)
-        {
-            currentMinutes += 1;
-            currentSeconds = 0;
+        if(!stopTimer)
+        {        
+            if(currentSeconds >= 60)
+            {
+                currentMinutes += 1;
+                currentSeconds = 0;
+            }
+            currentSeconds += 1 * Time.deltaTime;
+        
+            secondsTextBox.text = currentMinutes.ToString("0") + ":" +currentSeconds.ToString("00");
         }
-        currentSeconds += 1 * Time.deltaTime;
-      
-        secondsTextBox.text = currentMinutes.ToString("0") + ":" +currentSeconds.ToString("00");
-      
     }
 }
